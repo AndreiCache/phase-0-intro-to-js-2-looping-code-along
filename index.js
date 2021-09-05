13e633939f79ccfff2ee73e251584ad187a4e7b7
Andrei <giurrgiu@gmail.com>
2021-09-05 13:34:40 -0400

    Assigment Completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+
+
+
+
+
+
+function writeCards(guests, myparty) {
+  let message = [];
+  for(let i = 0; i < guests.length; i++) {
+    message.push(`Thank you, ${guests[i]}, for the wonderful surprise gift!`);
+  }
+return message;
+}
+
+
+function countDown(getReady) {
+  while (getReady >= 0) {
+    console.log(getReady);
+    getReady--;
+  }
+}
+
+  
+ 
